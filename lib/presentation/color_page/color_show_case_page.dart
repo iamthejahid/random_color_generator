ff8a7780de7be0f0047b9bf26e908060c5d9d3b0
xahid71 <jahid.info97@gmail.com>
2023-05-16 13:45:26 +0600

    update : managing state of color show case page, this fix #6

--- lib/presentation/color_page/color_show_case_page.dart
+++ lib/presentation/color_page/color_show_case_page.dart
@@ -1,67 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:random_color_generator/core/dependency_injector_contain.dart';
 import 'package:random_color_generator/core/font_size.dart';
 import 'package:random_color_generator/presentation/color_page/multi_color_show_case_page.dart';
-// import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 /// The Single Color Showing Page.
-class ColorShowCasePage extends StatelessWidget {
+class ColorShowCasePage extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final Size screenSize = MediaQuery.of(context).size;
 
+    final currentSate = ref.watch(colorStateNotifierProvider);
+    final controller = ref.read(colorStateNotifierProvider.notifier);
+
     return Scaffold(
-      backgroundColor: Colors.purpleAccent,
-      body: Stack(
-        children: [
-          SizedBox(
-            width: double.infinity,
-            height: double.infinity,
-            child: SizedBox(
-              height: screenSize.height,
-              width: screenSize.width,
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "Hello there",
-                      style: TextStyle(
-                        fontSize: FONT_SIZE,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    MaterialButton(
-                      color: Colors.red,
-                      child: const Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "view the multi color show case",
-                            style: TextStyle(
-                              fontSize: FONT_SIZE,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                          Icon(
-                            Icons.arrow_forward,
-                            color: Colors.white,
-                          )
-                        ],
-                      ),
-                      onPressed: () => Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => MultiColorShowCasePage(),
+      body: GestureDetector(
+        onTap: () => controller.colorChange(),
+        child: Container(
+          decoration: BoxDecoration(
+            color: currentSate.bgColor,
+          ),
+          height: screenSize.height,
+          width: screenSize.width,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Hello there",
+                  style: TextStyle(
+                    fontSize: FONT_SIZE,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                MaterialButton(
+                  color: currentSate.buttonColor,
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "view the multi color show case",
+                        style: TextStyle(
+                          fontSize: FONT_SIZE,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
                       ),
-                    )
-                  ],
-                ),
-              ),
+                      Icon(
+                        Icons.arrow_forward,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                  onPressed: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => MultiColorShowCasePage(),
+                    ),
+                  ),
+                )
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
