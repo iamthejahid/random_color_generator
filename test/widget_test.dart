98cefbaf2ab039ea1663c6330432f443548dc5d1
xahid71 <jahid.info97@gmail.com>
2023-05-16 09:49:12 +0600

    update : solving lint related issue and and re push for #1

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,47 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:random_color_generator/core/font_size.dart';
+import 'package:random_color_generator/presentation/color_page/color_show_case_page.dart';
+import 'package:random_color_generator/presentation/color_page/multi_color_show_case_page.dart';
 
 void main() {
-  if (kDebugMode) {
-    print('test will be soon!');
-  }
+  testWidgets('ColorShowCasePage arrived', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ColorShowCasePage(),
+      ),
+    );
+
+    // Verify that the page renders correctly
+    expect(find.text('Hello there'), findsOneWidget);
+    expect(find.text('view the multi color show case'), findsOneWidget);
+  });
+
+  testWidgets('Navigating to MultiColorShowCasePage',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ColorShowCasePage(),
+      ),
+    );
+
+    // Tap the MaterialButton to navigate to MultiColorShowCasePage
+    await tester.tap(find.byType(MaterialButton));
+    await tester.pumpAndSettle();
+
+    // Verify that MultiColorShowCasePage is displayed
+    expect(find.byType(MultiColorShowCasePage), findsOneWidget);
+  });
+
+  testWidgets('MultiColorShowCasePage renders correctly',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: MultiColorShowCasePage(),
+      ),
+    );
+
+    // Verify that the page renders correctly
+    expect(find.byType(Container), findsNWidgets(GRIDS_AMOUNT));
+  });
 }
